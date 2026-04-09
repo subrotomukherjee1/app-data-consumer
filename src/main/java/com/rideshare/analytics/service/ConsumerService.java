7ec020964c0cd092d66106d9d76476afce771d7d
subro <subroto.mukherjee1@gmail.com>
2026-04-09 11:50:57 +0100

    First Commit

---
+++ src/main/java/com/rideshare/analytics/service/ConsumerService.java
@@ -0,0 +1,11 @@
+package com.rideshare.analytics.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+@Service
+public class ConsumerService {
+    @KafkaListener(topics = "demo-topic", groupId = "demo-group")
+    public void consume(String message ) {
+        System.out.println("Consumed message: " + message);
+    }
+}
