f3742f22287d1ed1cd53c3b23d5c00c893c4744b
subro <subroto.mukherjee1@gmail.com>
2026-04-13 19:32:31 +0100

    Deployment Commit 1

--- src/main/java/com/rideshare/analytics/service/ConsumerService.java
+++ src/main/java/com/rideshare/analytics/service/ConsumerService.java
@@ -2,6 +2,7 @@ package com.rideshare.analytics.service;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
 @Service
 public class ConsumerService {
     @KafkaListener(topics = "demo-topic", groupId = "demo-group")
